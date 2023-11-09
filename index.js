ec38ff24e2e5b240bf95a53c05c9991f9d30c0d5
EA - Cauã Carvalho De Oliveira <caua_carvalho01@educadventista.org>
2023-11-09 15:43:44 -0300

    Recebendo informacões do usuario

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 const app = express();
 
@@ -5,12 +6,25 @@ const path = require("path");
 
 const caminhoBase = path.join(__dirname,"templates")
 
+app.use(express.urlencoded({
+    extended: true,
+}))
+app.use(express.json())
+
 app.listen(3000,()=>{
     console.log("Server running port 3000")
 })
 
 app.get('/',(req,res)=>{
     res.sendFile(`${caminhoBase}/index.html`)
+})
+
+app.get("/cadastrar",(req,res)=>{
+    res.sendFile(`${caminhoBase}/cadastro.html`)
+})
+app.post("/cadastrar/salvar",(req,res)=>{
+    const {nome,email,senha}= req.body
+    console.log(nome,senha,email)
 })
 app.get("/user/:id",(req,res)=>{
     const id = req.params.id;
